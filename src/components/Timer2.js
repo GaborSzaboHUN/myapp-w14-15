e40a356d15c77e17077c1fbc27ed86648bea0bca
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2022-06-30 12:20:29 +0200

    end of session 2022-06-30

--- src/components/Timer2.js
+++ src/components/Timer2.js
@@ -3,42 +3,63 @@
 import React, { useEffect, useState } from 'react'
 
 export default function Timer2() {
-    function incrementTimer() {
-        setTime(
-            time + 1
-        )
-    }
 
     const [time, setTime] = useState(0)
+    const [countryName, setCountryName] = useState("")
+    const [country, setCountry] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setLoading(true)
+        fetch(`https://restcountries.com/v3.1/name/hungary`)
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data)
+                setLoading(false)
+            })
+    }, [])
 
     useEffect(() => {
         // console.log("useEffect called")
 
-        console.log("New interval set")
+        console.log("Time changed")
 
-        const intervalId = setInterval(() => {
-            incrementTimer()
-        }, 1000)
+    }, [time])
 
-        return () => {
-            clearInterval(intervalId)
+
+    useEffect(() => {
+        async function fetchData() {
+            // you await here
+            let response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+            const data = await response.json()
+            setCountry(data[0].name.nativeName[data[0].cca3.toLowerCase()].common)
         }
 
-    })
+        if (countryName !== "") {
+            fetchData();
+        }
+
+    }, [countryName])
+
 
     return (
-        <>
+        loading ? <div>Loading...</div> :
+            <>
 
-            <h1>
-                {time}
-            </h1>
+                <h1>
+                    {time}
+                </h1>
 
-            {/* 
+                {/* 
             <button onClick={() => { incrementTimer() }}>Click me</button>
             */}
 
-            <button onClick={incrementTimer}>Click me</button>
+                <input type="text" onChange={(event) => { setCountryName(event.target.value) }}></input>
 
-        </>
+                <button onClick={() => { setTime(time + 1) }}>Click me</button>
+
+                <div>{country}</div>
+
+            </>
     )
 }
